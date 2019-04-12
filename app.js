a6fb3b735eac70ef1e823fd436b650c5d639f197
andy (Giussepe) <xandyx2014@gmail.com>
2019-04-12 14:11:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(cors());
 
 app.use('/test', require('./routes/test.route'));
